8a6312e37bd83e6735eefbb5fa729cdc65b5c1ec
Leeh <leticiasalustiano07@gmail.com>
2024-11-01 11:30:27 -0300

    .

--- script.js
+++ script.js
@@ -8,3 +8,27 @@ menubutton.addEventListener("click", function() {
         menu.style.display = "block";
     }
 });
+
+
+//carrossel
+const pessoas = document.querySelector('.pessoas');
+const btnEsquerda = document.getElementById('btn-esquerda');
+const btnDireita = document.getElementById('btn-direita');
+
+let index = 0;
+
+function updateCarousel() {
+    const totalImages = pessoas.children.length;
+    const offset = index * -105; 
+    pessoas.style.transform = `translateX(${offset}px)`;
+}
+
+btnEsquerda.addEventListener('click', () => {
+    index = (index > 0) ? index - 1 : pessoas.children.length - 1;
+    updateCarousel();
+});
+
+btnDireita.addEventListener('click', () => {
+    index = (index < pessoas.children.length - 1) ? index + 1 : 0;
+    updateCarousel();
+});
